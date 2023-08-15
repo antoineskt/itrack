34f355f3a9b44cb7988ab03ccedd9a7310107dc6
Raphael <guet.raphael@hotmail.com>
2023-08-15 17:37:25 +0200

    Ajout de la 4eme section, partie card

---
+++ components/data.ts
@@ -0,0 +1,44 @@
+export type PriceDataType = {
+  title: string;
+  price: string;
+  description: string[];
+};
+
+export const priceData: PriceDataType[] = [
+  {
+    title: 'Free',
+    price: 'Gratuite',
+    description: [
+      'First 5,000 page views',
+      'No coding required',
+      'Integrates with any themes',
+      'Create your own tracking page',
+      'Live chat support 24/7',
+      'Order lookup',
+    ],
+  },
+  {
+    title: 'GROWTH PLAN',
+    price: '$4.99/mois',
+    description: [
+      '50,000 page views per month',
+      'Integrates with any themes',
+      'Create your own tracking page',
+      'Live chat support 24/7',
+      'Order lookup',
+      'Custom URL',
+    ],
+  },
+  {
+    title: 'PRO PLAN',
+    price: '$9.99/mois',
+    description: [
+      'Unlimited views',
+      'Integrates with any themes',
+      'Create your own tracking page',
+      'Live chat support 24/7',
+      'Order lookup',
+      'Custom URL',
+    ],
+  },
+];
